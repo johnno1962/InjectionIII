dfa4e4b7e93acf54b677122245e432e0b4cdf1f5
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-06-06 18:27:04 +0200

    Bump file $Id

--- InjectionBundle/SwiftInjection.swift
+++ InjectionBundle/SwiftInjection.swift
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 05/11/2017.
 //  Copyright © 2017 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/ResidentEval/InjectionBundle/SwiftInjection.swift#44 $
+//  $Id: //depot/ResidentEval/InjectionBundle/SwiftInjection.swift#45 $
 //
 //  Cut-down version of code injection in Swift. Uses code
 //  from SwiftEval.swift to recompile and reload class.
