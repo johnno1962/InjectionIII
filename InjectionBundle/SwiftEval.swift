87d20d6b1918ca083cc1efb1a77fcc096c7692b5
John Holdsworth <github@johnholdsworth.com>
2018-10-30 10:45:13 +0000

    Log files as they are stored up for injection.

--- InjectionBundle/SwiftEval.swift
+++ InjectionBundle/SwiftEval.swift
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 02/11/2017.
 //  Copyright © 2017 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/ResidentEval/InjectionBundle/SwiftEval.swift#110 $
+//  $Id: //depot/ResidentEval/InjectionBundle/SwiftEval.swift#111 $
 //
 //  Basic implementation of a Swift "eval()" including the
 //  mechanics of recompiling a class and loading the new
