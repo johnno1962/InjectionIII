0ce22a2196d04761671fe4598a00d14c31a67a59
John Holdsworth <github@johnholdsworth.com>
2020-05-08 17:34:36 +0200

    Source back under control (#228)

--- InjectionIII/AppDelegate.swift
+++ InjectionIII/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 06/11/2017.
 //  Copyright © 2017 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/ResidentEval/InjectionIII/AppDelegate.swift#11 $
+//  $Id: //depot/ResidentEval/InjectionIII/AppDelegate.swift#12 $
 //
 
 import Cocoa
