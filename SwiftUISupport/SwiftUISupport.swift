72c75aacf6cb54ada185e1db5f47f80021e467c5
John Holdsworth <github@johnholdsworth.com>
2020-12-01 20:14:16 +0100

    Trace, decorating any type.

--- SwiftUISupport/SwiftUISupport.swift
+++ SwiftUISupport/SwiftUISupport.swift
@@ -5,26 +5,21 @@
 //  Created by John Holdsworth on 25/09/2020.
 //  Copyright © 2020 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/ResidentEval/SwiftUISupport/SwiftUISupport.swift#10 $
+//  $Id: //depot/ResidentEval/SwiftUISupport/SwiftUISupport.swift#13 $
 //
 
 import SwiftUI
 import SwiftTrace
 
 extension SwiftUI.EdgeInsets: SwiftTraceFloatArg {}
+extension SwiftUI.UnitPoint: SwiftTraceFloatArg {}
 
 @objc (SwiftUISupport)
 class SwiftUISupport: NSObject {
 
     @objc class func setup(pointer: UnsafeMutableRawPointer?) {
-
         print("💉 Installed SwiftUI type handlers")
-        SwiftTrace.addFormattedType(SwiftUI.Text.self)
-        SwiftTrace.addFormattedType(SwiftUI.Color.self)
-        SwiftTrace.addFormattedType(SwiftUI.Image.self)
-        SwiftTrace.addFormattedType(SwiftUI.Edge.Set.self)
-        SwiftTrace.addFormattedType(SwiftUI.Alignment.self)
-        SwiftTrace.addFormattedType(SwiftUI.EdgeInsets.self)
-        SwiftTrace.addFormattedType(SwiftUI.LocalizedStringKey.self)
+        SwiftTrace.makeTraceable(types: [SwiftUI.Edge.Set.self,
+                                         SwiftUI.Binding<Swift.Bool>.self])
     }
 }
