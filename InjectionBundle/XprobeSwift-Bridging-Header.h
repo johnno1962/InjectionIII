a0c3ca3bfe1c49aed93aae033de9be50883fe1de
John Holdsworth <github@johnholdsworth.com>
2021-03-24 07:30:56 +0100

    Eliminate Xtrace.

--- InjectionBundle/XprobeSwift-Bridging-Header.h
+++ InjectionBundle/XprobeSwift-Bridging-Header.h
@@ -2,15 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "SimpleSocket.h"
 #import "InjectionClient.h"
-#import "UserDefaults.h"
-#import "../XprobePlugin/Sources/Xprobe/include/Xtrace.h"
 #import "../XprobePlugin/Sources/Xprobe/include/Xprobe.h"
-
-@interface NSObject(InjectionSweep)
-- (void)bsweep;
-@end
-
-@interface NSObject(RunXCTestCase)
-+ (void)runXCTestCase:(Class)aTestCase;
-@end
