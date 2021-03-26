1b41d699f65a06cc40cd64e2179e364837563801
John Holdsworth <github@johnholdsworth.com>
2021-03-26 18:57:21 +0100

    Post release (#307)
    * Streaming Video Capture
    
    * Eliminate Xtrace.
    
    * Remove a couple more compilation warnings.
    
    * Renovate Object Graphing.
    
    * Bump version.

--- InjectionBundle/XprobeSwift-Bridging-Header.h
+++ InjectionBundle/XprobeSwift-Bridging-Header.h
@@ -2,9 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "InjectionClient.h"
+#import "SimpleSocket.h"
 #import "UserDefaults.h"
-#import "../XprobePlugin/Sources/Xprobe/include/Xtrace.h"
+#import "InjectionClient.h"
 #import "../XprobePlugin/Sources/Xprobe/include/Xprobe.h"
 
 @interface NSObject(InjectionSweep)
