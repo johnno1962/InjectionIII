78da7c4653ef2a7af30d37d08ebe635f88e3d04f
John Holdsworth <github@johnholdsworth.com>
2023-12-04 15:20:00 +0100

    Copy various bundles.

--- InjectionIII/copy_bundle.sh
+++ InjectionIII/copy_bundle.sh
@@ -6,17 +6,28 @@
 #  Copies injection bundle for on-device injection.
 #  Thanks @oryonatan
 #
+#  $Id: //depot/ResidentEval/InjectionIII/copy_bundle.sh#4 $
+#
+
 if [ "$CONFIGURATION" == "Debug" ]; then
     RESOURCES="$(dirname "$0")"
-    if [ "$PLATFORM_NAME" == "iphonesimulator" ]; then
+    COPY="$CODESIGNING_FOLDER_PATH/iOSInjection.bundle"
+    STRACE="$COPY/Frameworks/SwiftTrace.framework/SwiftTrace"
+    PLIST="$COPY/Info.plist"
+    if [ "$PLATFORM_NAME" == "macosx" ]; then
+     BUNDLE=${1:-macOSInjection}
+     COPY="$CODESIGNING_FOLDER_PATH/Contents/Resources/macOSInjection.bundle"
+     STRACE="$COPY/Contents/Frameworks/SwiftTrace.framework/Versions/A/SwiftTrace"
+     PLIST="$COPY/Contents/Info.plist"
+    elif [ "$PLATFORM_NAME" == "appletvsimulator" ]; then
+     BUNDLE=${1:-tvOSInjection}
+    elif [ "$PLATFORM_NAME" == "iphonesimulator" ]; then
      BUNDLE=${1:-iOSInjection}
     else
      BUNDLE=${1:-maciOSInjection}
     fi
-    COPY="$CODESIGNING_FOLDER_PATH/iOSInjection.bundle"
     rsync -a "$RESOURCES/$BUNDLE.bundle"/* "$COPY/" &&
-    /usr/libexec/PlistBuddy -c "Add :UserHome string $HOME"  "$COPY/Info.plist" &&
-    codesign -f --sign "$EXPANDED_CODE_SIGN_IDENTITY" --timestamp\=none --preserve-metadata\=identifier,entitlements,flags --generate-entitlement-der "$COPY/Frameworks/SwiftTrace.framework/SwiftTrace" &&
-    codesign -f --sign "$EXPANDED_CODE_SIGN_IDENTITY" --timestamp\=none --preserve-metadata\=identifier,entitlements,flags --generate-entitlement-der "$COPY" &&
+    /usr/libexec/PlistBuddy -c "Add :UserHome string $HOME" "$PLIST" &&
+    codesign -f --sign "$EXPANDED_CODE_SIGN_IDENTITY" --timestamp\=none --preserve-metadata\=identifier,entitlements,flags --generate-entitlement-der "$STRACE" "$COPY" &&
     defaults write com.johnholdsworth.InjectionIII "$PROJECT_FILE_PATH" $EXPANDED_CODE_SIGN_IDENTITY
 fi
